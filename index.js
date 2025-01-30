ce684b70a49e1c1d55c61828f4391f81e7c97bdd
Daren Morreale <daren@givelegacy.com>
2025-01-30 15:40:11 -0500

    Create function to render the freelancer array to the DOM

--- index.js
+++ index.js
@@ -36,9 +36,32 @@ function init() {
   thead.append(header_row);
   table.append(thead);
   table.append(tbody);
-
   root.append(table);
 
+  freelancerArray(); 
+}
+
+function freelancerArray() {
+    const freelancerTable = document.querySelector('tbody');
+
+    const freelancerElements = freelancers.map(freelancer => {
+        const row = document.createElement('tr');
+
+        const name = document.createElement('td');
+        name.textContent = freelancer.name;
+
+        const occupation = document.createElement('td');
+        occupation.textContent = freelancer.occupation;
+
+        const price = document.createElement('td');
+        price.textContent = freelancer.price;
+
+        row.append(name, occupation, price);
+
+        return row;
+    });
+
+    freelancerTable.replaceChildren(...freelancerElements);
 }
 
 init();
